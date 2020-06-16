3286d09b65c3a0eb5427ad7cd68f7954789512bb
Anthony <chitowncoder@gmail.com>
2020-06-16 18:01:27 -0500

    intital commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './utils/PrivateRoute';
+
+import Login from './components/Login';
+import './styles.scss';
+
+function App() {
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/" component={Login} />
+				<PrivateRoute path="/bubble-page" component={BubblePage} />
+			</div>
+		</Router>
+	);
+}
+
+export default App;
